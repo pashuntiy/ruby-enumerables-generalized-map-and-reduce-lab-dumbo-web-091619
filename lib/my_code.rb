1ebdac32227ae98a67eaf205a5a4da27042a2e05
Pavel Ilin <pavel_ilin@yahoo.com>
2019-09-01 21:39:10 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,14 +1,8 @@
-# Your Code 
-def (source_array)
-   source_array.map() {|n| n * -1}
-
-
-
 def map_to_negativize(source_array)
   new = []
   i = 0
   while i < source_array.length
-    new.push(yield(s[i]))
+    new.push(yield(source_array[i]))
     i += 1
   end
   new
