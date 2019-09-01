3ac5eebb2d21ef81a87ba679b5d75f302c230e34
Pavel Ilin <pavel_ilin@yahoo.com>
2019-09-01 21:44:50 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,13 +1,13 @@
 source_array=[1,23,4,5,7,12]
 
 def map(source_array)
-  new=[]
+  new_array=[]
   i=0 
   while i<source_array.length
     new.push(yield(source_array[i]))
     i+=1
   end
-  return new
+  return new_array
 end
 
 map(source_array){|n| n*-1}
