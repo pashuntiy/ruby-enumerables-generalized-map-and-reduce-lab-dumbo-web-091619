bbba48def50a946e43ff0953b29b13baa073d44a
Pavel Ilin <pavel_ilin@yahoo.com>
2019-09-01 21:49:43 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -12,3 +12,14 @@ end
 
 map(source_array){|n| n*-1}
 
+def reduce(source_array,starting_point=0)
+  i=0
+  while i <source_array.length
+  starting_point = yield(starting_point,source_array[i])
+  i+=1
+  end
+  if starting_point==0 
+    starting_point=true
+  end
+  return starting_point
+end
